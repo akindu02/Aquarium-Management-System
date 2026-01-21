8fd6bafdb030177972475d1198d817e9ed3d8a68
Akindu Samarasinghe <samaras-im22022@stu.kln.lk>
2026-01-21 19:00:33 +0530

    server.js port change

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const authRoutes = require('./routes/authRoutes');
 const { pool } = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 /**
  * Middleware
